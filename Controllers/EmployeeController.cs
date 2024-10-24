56df90e3bf6676cbaa5d1e1b2b8db06d373abe72
Luyanda Mazibuko <ST10176153@rcconnect.edu.za>
2024-10-24 16:53:50 +0000

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,149 @@
+﻿using EmployeeData.DAL;
+using EmployeeData.Models;
+using EmployeeData.Models.DBEntities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeData.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly EmployeeDbContext _context;
+        public EmployeeController(EmployeeDbContext context)
+        {
+            this._context = context;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var employees = _context.Employees.ToList();
+            List<EmployeeViewModel> employeeList = new List<EmployeeViewModel>();
+            if (employees != null)
+            {
+
+                foreach (var employee in employees)
+                {
+                    var EmployeeViewModel = new EmployeeViewModel()
+                    {
+                        Id = employee.Id,
+                        FirstName = employee.FirstName,
+                        LastName = employee.LastName,
+                        DateofBirth = employee.DateofBirth,
+                        Email = employee.Email,
+                        Salary = employee.Salary,
+                    };
+                    employeeList.Add(EmployeeViewModel);
+
+                }
+                return View(employeeList);
+            }
+            return View(employeeList);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(EmployeeViewModel employeeData)
+
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var employee = new Employee()
+                    {
+                        FirstName = employeeData.FirstName,
+                        LastName = employeeData.LastName,
+                        DateofBirth= employeeData.DateofClaim,
+                        Email = employeeData.Email,
+                        Salary = employeeData.Salary,
+                    };
+                    _context.Employees.Add(employee);
+                    _context.SaveChanges();
+                    TempData["successMessage"] = "Claim was successfully entered";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    TempData["errorMessage"] = "Model data is not valid";
+                    return View();
+                }
+            }
+            catch (Exception ex)
+
+            {
+
+                TempData["errorMessage"] = ex.Message;
+                return View();
+            }
+        }
+        [HttpGet]
+        public IActionResult Edit(int Id)
+        {
+            try
+            {
+                var employee = _context.Employees.SingleOrDefault(x => x.Id == Id);
+                if (employee != null)
+                {
+
+                    var employeeView = new EmployeeViewModel()
+                    {
+                        Id = employee.Id,
+                        FirstName = employee.FirstName,
+                        LastName = employee.LastName,
+                        DateofClaim = employee.DateofBirth,
+                        Email = employee.Email,
+                        Salary = employee.Salary,
+                    };
+                    return View(employeeView);
+                }
+                else
+                {
+                    TempData["errorMessage"] = $"Employee Details not available with Id:  {Id}";
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                TempData["errorMessage"]= ex.Message;
+                return RedirectToAction("Index");
+            }
+        }
+        [HttpPost]
+        public IActionResult Edit(EmployeeViewModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var employee = new Employee()
+                    {
+                        Id = model.Id,
+                        FirstName = model.FirstName,
+                        LastName = model.LastName,
+                        DateofBirth = model.DateofClaim,
+                        Email = model.Email,
+                        Salary = model.Salary,
+                    };
+                    _context.Employees.Update(employee);
+                    _context.SaveChanges();
+                    TempData["successMessage"] = "Claim has been approved";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    TempData["errorMessage"] = "Claim has not been approved";
+                    return RedirectToAction();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                TempData["errorMessage"] = ex.Message;
+                return RedirectToAction();
+            }
+        }
+    }
+}
---
+++ Models/EmployeeViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmployeeData.Models
+{
+    public class EmployeeViewModel
+    {
+        public int Id { get; set; }
+        [DisplayName("FirstName")]
+        public string FirstName { get; set; }
+        [DisplayName("LastName")]
+        public string LastName { get; set; }
+        [DisplayName("HoursWorked")]
+        public int HoursWorked { get; set; }
+        [DisplayName("Date of Birth")]
+        public DateTime DateofBirth { get; set; }
+        [DisplayName("Email")]
+        public string Email { get; set; }
+        public double Salary { get; set; }
+        [DisplayName("Name")]
+        public string FullName
+        { 
+            get { return FirstName + " " + LastName; }
+        }
+    }
+}
+
+
